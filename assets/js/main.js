1741da4f3ca9703a7803486f7f83940a44d90345
LinhTQ <linhtq@vtconline.vn>
2024-12-24 17:31:46 +0700

    add: pop-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[type="scroll-to-section"]');
+
+    for (const link of links) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 124,
+                    behavior: "smooth"
+                });
+            }
+        });
+    }
+});
