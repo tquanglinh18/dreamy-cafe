9ae817ab3a69ddc645833b02074ca43807a93975
LnhTQ <linhtq@vtconline.vn>
2024-12-31 00:52:21 +0700

    add: feedback user and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
 $(function () {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
-      });
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow'),
+    responsive: [
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
 
 
