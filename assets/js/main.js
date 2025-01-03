4d9523db1554b5338712e63879e13615ea289399
LinhTQ <linhtq@vtconline.vn>
2025-01-03 13:20:45 +0700

    ad: noi that section

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,21 @@ $(function () {
   $("#btn-show-more").click(() => {
     $(".pined-quick-action").toggleClass("show-quick-action");
   })
+
+  autoPlaySlide();
+
+  function autoPlaySlide() {
+    const lstSlide = document.querySelectorAll('.noithat-item');
+    console.log(typeof lstSlide);
+
+    const slideCurrentElm = document.querySelector('.noithat-item.noithat-item--active');
+    console.log(slideCurrentElm);
+
+    const idxCurrentSlide = lstSlide.index(slideCurrentElm);
+    console.log(idxCurrentSlide);
+  }
 });
+
+
 
 
