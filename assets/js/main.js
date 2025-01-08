4f8a2b2c3ecca6b695415909333843fe2874463a
LinhTQ <linhtq@vtconline.vn>
2025-01-08 17:00:24 +0700

    add: giftcode

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,12 +29,36 @@ $(function () {
     $(".pined-quick-action").toggleClass("show-quick-action");
   })
 
-  // const lstOptionAccount = $("#server-1")
-  // lstOptionAccount.click(() => {
-  //   console.log(lstOptionAccount.text());
-  //   $("#sel-account").text(lstOptionAccount.text())
-  //   $(".dropdown-menu").removeClass("show");
-  // })
+  const lstOptionAccount = $("#input-gift-code .dropdown-item")
+  for (let i = 0; i < lstOptionAccount.length; i++) {
+    const idElmSel = '#account-'.concat(i + 1)
+    $(idElmSel).click(() => {
+      console.log(idElmSel)
+      $("#sel-account-type").text($(idElmSel).text())
+      $("#input-gift-code .dropdown-menu").removeClass("show");
+    })
+  }
+
+  const lstServices = $("#lst-payment-method .dropdown-item")
+  for (let i = 0; i < lstServices.length; i++) {
+    const idServieceSel = "#".concat($(lstServices[i]).attr("id"))
+
+    $(idServieceSel).click(() => {
+      $("#lst-payment-method #sel-services").text($(idServieceSel).text())
+      $("#lst-payment-method #sel-services ~ .dropdown-menu").removeClass("show");
+    })
+  }
+
+  const lstPearlElm = $("#sel-pearl .dropdown-item")
+  for (let i = 0; i < lstPearlElm.length; i++) {
+    const idPearlSel = "#".concat($(lstServices[i]).attr("id"))
+    const idPearlSelCount = idPearlSel.concat(" .number-of-pearls--name")
+    console.log(idPearlSel)
+    $(idPearlSel).click(() => {
+      $("#sel-pearl #sel-coin .number-of-pearls--name").text($(idPearlSelCount).text())
+      $("#sel-pearl #sel-coin ~ .dropdown-menu").removeClass("show");
+    })
+  }
 
 });
 
